e54149b87333368d5d3ff2302839332dc11bc994
82240953 <jhnamgung@kakao.com>
2022-06-24 15:34:36 +0900

    testB commit-1,2

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Hello testA!")
+print("Hello testB!")
 
 print("Main commit 1")
 
@@ -7,3 +7,5 @@ print("Main commit 2")
 print("Main commit 3")
 
 print("testA commit 2")
+
+print("testB commit 2")
